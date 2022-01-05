32bf09da0facb4df2d93b497bd6be695592fba80
Alisher <alisher1929@umail.uz>
2022-01-06 00:56:50 +0500

    start app

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// By @AbubakerSaeed96 (twitter)
+// ===================
+
+// Images are from Unsplash.
+
+// Looks Good In Firefox
+// Font not displaying correctly in Chrome and Opera
+// Looks Good In Edge Dev (Almost)
+
+// Thank You For Viewing.
+
+const cart1 = document.querySelector(".flip-card-container");
+
+cart1.addEventListener("click", (e) => {
+    
+    cart1.classList.toggle("show");
+});
